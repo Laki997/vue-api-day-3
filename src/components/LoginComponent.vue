babdabe064a6923e60272c0297093d507270b51c
Lazar Kalajdzic <laki.kalajdzic@gmail.com>
2021-06-04 10:09:36 +0200

    login logout

---
+++ src/components/LoginComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <form @submit.stop.prevent="login()">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+          v-model="user.email"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          class="form-control"
+          id="password"
+          aria-describedby="emailHelp"
+          placeholder="Enter password"
+          v-model="user.password"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { authService } from "../services/auth.service";
+export default {
+  name: "LoginComponent",
+  data() {
+    return {
+      user: {},
+    };
+  },
+
+  methods: {
+    login() {
+      authService.login(this.user).then((response) => {
+        console.log(response);
+        this.$router.push("/");
+      });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import LoginComponent from "../components/LoginComponent";
+import { globalAuthGuard } from "../guards/auth.guard";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "/about",
+    meta: { authRequired: true },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginComponent,
+  },
+
+  {
+    path: "/about",
+    name: "About",
+    meta: { authRequired: true },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.beforeEach(globalAuthGuard);
+
+export default router;
